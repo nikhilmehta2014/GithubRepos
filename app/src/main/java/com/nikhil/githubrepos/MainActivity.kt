c5d36697daa9a3bf59959f4140e02a98b1fc3f40
Nikhil <nikhilmht33@gmail.com>
2020-04-09 21:55:59 +0530

    Setup

---
+++ app/src/main/java/com/nikhil/githubrepos/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nikhil.githubrepos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
